d7d6f262d81400a9e8572ba48f6e8a6864a491dc
OnnoH <ohuijgen@gmail.com>
2025-07-28 09:14:46 +0200

    Model

---
+++ src/main/java/nl/onnoh/bdg/bpmn/model/common/ElementTemplate.java
@@ -0,0 +1,11 @@
+package nl.onnoh.bdg.bpmn.model.common;
+
+import lombok.Data;
+
+
+@Data
+public class ElementTemplate {
+    private String name;
+    private String version;
+    private String icon;
+}
